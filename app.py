17bcb9efc33e1e79a7c86594a9a770a87f63854a
itsthomashere <thomasadam.leigh@gmail.com>
2023-10-30 14:37:33 +0200

    Updating save_audio

--- app.py
+++ app.py
@@ -31,7 +31,10 @@ def main() -> None:
 
         if video_url and generate:
             with st.spinner("Generating"):
-                step_1.save_audio(video_url)
+                video_title, save_location, thumbnail_url = step1.save_audio(video_url)
+                st.header(video_title)
+                st.image(thumbnail_url)
+                st.audio(save_location)
 
 
     with col2:
--- src/youtube.py
+++ src/youtube.py
@@ -10,8 +10,8 @@ def save_audio(url):
 	base, ext = os.path.splitext(out_file)
 	file_name = base + '.mp3'
 	os.rename(out_file, file_name)
-	st.write(yt.title + " has been successfully downloaded.")
-	st.write(file_name)
+	print(yt.title + " has been successfully downloaded.")
+	print(file_name)
 	return yt.title, file_name, yt.thumbnail_url
 
 
