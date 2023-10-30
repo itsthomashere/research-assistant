b5424256d547d4c681ee12a562b8fab527465360
itsthomashere <thomasadam.leigh@gmail.com>
2023-10-30 13:04:33 +0200

    Adding columns

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 def main() -> None:
     st.title("AI Research Assistant")
+    col1, col2 = st.columns([2, 1])
+    with col1:
+        st.info("Block 1")
+
+    with col2:
+        st.info("Block 2")
 
 if __name__ == "__main__":
     main()
