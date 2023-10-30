ce69ef29955c8021555bfd2cd90c6817f01ec3fe
itsthomashere <thomasadam.leigh@gmail.com>
2023-10-30 13:03:01 +0200

    Adding title

--- app.py
+++ app.py
@@ -9,5 +9,8 @@ import streamlit as st
 aai.settings.api_key = st.secrets["ASSEMBLYAI_TOKEN"]
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+def main() -> None:
+    st.title("AI Research Assistant")
+
 if __name__ == "__main__":
-    pass
+    main()
